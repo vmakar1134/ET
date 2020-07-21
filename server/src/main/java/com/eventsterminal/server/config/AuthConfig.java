a2dceff7e0f6857eb290c7ffb4b4a8130638154a
vmakar1134 <vmakar1134@gmail.com>
2020-07-21 15:36:22 -0700

    add oauth impl. need refactor

--- server/src/main/java/com/eventsterminal/server/config/AuthConfig.java
+++ server/src/main/java/com/eventsterminal/server/config/AuthConfig.java
@@ -1,24 +1,122 @@
 package com.eventsterminal.server.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+@PropertySource("application-oauth2.properties")
 public class AuthConfig extends WebSecurityConfigurerAdapter {
+
+    private static List<String> clients = Arrays.asList("google", "facebook");
+
+    private static final String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
+
+    private final Environment env;
+
+    public AuthConfig(Environment env) {
+        this.env = env;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // TODO: 7/21/20 refactor endpoints
         http.authorizeRequests()
-                .antMatchers("/login", "/test/**")
+                .antMatchers("/login", "/loginFailure")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2Login()
-                .loginPage("/login");
+                .loginPage("/login")
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorize-client")
+                .authorizationRequestRepository(authorizationRequestRepository())
+                .and()
+                .tokenEndpoint()
+                .accessTokenResponseClient(accessTokenResponseClient())
+                .and()
+                .defaultSuccessUrl("/login_success")
+                .failureUrl("/loginFailure")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                .clearAuthentication(true)
+                .and()
+                .csrf()
+                .disable();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<ClientRegistration> registrations = clients.stream()
+                .map(this::getRegistration)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(
+                clientRegistrationRepository());
+    }
+
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest>
+    authorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest>
+    accessTokenResponseClient() {
+        return new DefaultAuthorizationCodeTokenResponseClient();
+    }
+
+    // TODO: 7/21/20 refactor null response
+    private ClientRegistration getRegistration(String client) {
+        // TODO: 7/21/20 refactor string
+        String clientId = env.getProperty(CLIENT_PROPERTY_KEY + client + ".client-id");
+        if (clientId == null) {
+            return null;
+        }
+        String clientSecret = env.getProperty(
+                CLIENT_PROPERTY_KEY + client + ".client-secret");
+
+        // TODO: 7/21/20 refactor if conditions
+        if (client.equals("google")) {
+            return CommonOAuth2Provider.GOOGLE.getBuilder(client)
+                    .clientId(clientId).clientSecret(clientSecret).build();
+        }
+        if (client.equals("facebook")) {
+            return CommonOAuth2Provider.FACEBOOK.getBuilder(client)
+                    .clientId(clientId).clientSecret(clientSecret).build();
+        }
+        return null;
     }
 
 }
---
+++ server/src/main/java/com/eventsterminal/server/controller/LoginController.java
@@ -0,0 +1,87 @@
+package com.eventsterminal.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO: 7/21/20 RestController
+@Controller
+public class LoginController {
+
+    // TODO: 7/21/20 to application-custom-properties
+    private static String authorizationRequestBaseUri = "oauth2/authorize-client";
+
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+
+    // TODO: 7/21/20 to constructor
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    // TODO: 7/21/20 to constructor
+    @Autowired
+    private OAuth2AuthorizedClientService authorizedClientService;
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        // TODO: 7/21/20 to service (probably to filter)
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                .as(Iterable.class);
+        if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+        clientRegistrations.forEach(registration -> oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "login";
+    }
+
+    @GetMapping("/login_success")
+    public String getLoginInfo(Model model, OAuth2AuthenticationToken authentication) {
+        // TODO: 7/21/20 to service (probably to filter)
+        OAuth2AuthorizedClient client = authorizedClientService
+                .loadAuthorizedClient(
+                        authentication.getAuthorizedClientRegistrationId(),
+                        authentication.getName());
+        String userInfoEndpointUri = client.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUri();
+
+        if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken()
+                    .getTokenValue());
+            HttpEntity entity = new HttpEntity("", headers);
+            ResponseEntity<Map> response = restTemplate
+                    .exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+            Map userAttributes = response.getBody();
+            model.addAttribute("name", userAttributes.get("name"));
+        }
+        return "login_success";
+    }
+
+    // TODO: 7/21/20 remove, for test only
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "HOME";
+    }
+
+}
+
